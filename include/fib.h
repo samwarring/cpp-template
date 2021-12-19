5dd9045912fe3f66c6d191b4e11f42409602ba20
Sam Warring <samwarring@gmail.com>
2021-12-19 08:14:21 -0800

    Use a slightly more complicated example program
    - library target fib: defines fibonacci function
    - executable target fib_test: boost unit-tets for fib
    - remove existing main.cpp file

---
+++ include/fib.h
@@ -0,0 +1,8 @@
+#ifndef INCLUDED_FIB_H
+#define INCLUDED_FIB_H
+
+/// Returns the nth number in the fibonacci sequence.
+/// fib(0) = 0; fib(1) = 1; fib(2) = 1; fib(3) = 2; etc.
+int fib(int n);
+
+#endif
---
+++ src/fib.cpp
@@ -0,0 +1,18 @@
+#include <fib.h>
+
+int fib(int n) {
+  if (n <= 0) {
+    return 0;
+  } else if (n <= 2) {
+    return 1;
+  }
+  int prev2 = 1;
+  int prev1 = 1;
+  int curr = 2;
+  for (n -= 3; n > 0; --n) {
+    prev2 = prev1;
+    prev1 = curr;
+    curr = prev2 + prev1;
+  }
+  return curr;
+}
